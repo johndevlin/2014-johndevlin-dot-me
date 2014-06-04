5e399cd063e34ae31eec9fa37c19bcf7b659c5ce
John Devlin <j@johndevlin.me>
2014-06-04 18:41:07 +0100

    Updated the project loader.

--- assets/production/js/production.js
+++ assets/production/js/production.js
@@ -383,10 +383,6 @@ $(window).load(function() {
 		$('.js footer').fadeIn(500);
 	}, 1600);
 
-	setTimeout(function() {
-		window.scrollReveal = new scrollReveal();
-	}, 1605);
-
 });
 
 // Add current year to .current-year
@@ -401,52 +397,6 @@ $(document).keyup(function(e) {
 	}
 
 });
-
-
-// Validation
-function validateEmail(email) {
-	var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-	return re.test(email);
-}
-
-function validation() {
-
-	var name = $('input[name=name]').val();
-	var email = $('input[name=email]').val();
-	var message = $('textarea[name=message]').val();
-
-	var err_name = 0, err_email = 0, err_message = 0;
-
-	if(name == '' || name == undefined || name.length < 6){
-		err_name = 1;
-		$('.name_err').addClass('show-error');
-	} else {
-		err_name = 0;
-		$('.name_err').removeClass('show-error');
-	}
-
-	if(email == '' || email == undefined || !validateEmail(email)){
-		err_email = 1;
-		$('.email_err').addClass('show-error');
-	} else {
-		err_email = 0;
-		$('.email_err').removeClass('show-error');
-	}
-
-	if(message == '' || message == undefined || message.length < 6){
-		err_message = 1;
-		$('.message_err').addClass('show-error');
-	} else {
-		err_message = 0;
-		$('.message_err').removeClass('show-error');
-	}
-
-	if(err_name == 0 && err_email == 0 && err_message == 0){
-		return true;
-	}
-
-	return false;
-}
 
 
 /* Smooth scroll */
